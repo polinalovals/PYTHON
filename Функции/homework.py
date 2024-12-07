89b7ef60bfffc459fc343cbbf874b511acc40d70
Polina <polina@macbook-air-4.speedport.ip>
2024-12-07 09:23:16 +0300

    homework решение задач

--- Функции/homework.py
+++ Функции/homework.py
@@ -12,3 +12,32 @@ def norm_number(a):
 
 
 print(norm_number('11001'))
+
+
+def recursiv_number(a=input('geben sie eine zahel ein: '))
+    for a == 0:
+        return 0
+    for a == 1:
+        return 1
+    return recursiv_number(int(elements) for elements in a)  # как то написать сумму
+
+
+def recursiv_dis(a):
+    if a < 10:
+        return a
+    else:
+        return a % 10 + recursiv_dis(a // 10)
+
+
+number = int(input("Geben Sie eine Zahl ein: "))
+result = recursiv_dis(number)
+
+
+def recursiv_dis(a=input('Geben Sie eine Zahl ein: ')):
+    if len(a) == 0:
+        return 0
+    return int(a[0]) + recursiv_dis(a[1:])
+
+
+result = recursiv_dis()
+print(f"Die Summe der Ziffern ist: {result}")
