5066b5b3201afd147eba2f85694fe5f5cb5d8a1b
Polina <polina@macbook-air-4.speedport.ip>
2024-12-07 09:16:52 +0300

    homework решение задач

--- Функции/homework.py
+++ Функции/homework.py
@@ -1,2 +1,14 @@
 # Написать рекурсивную функцию, переводящую число в двоичное представление
 # Предварительно написать функцию нерекурсивную с этим алгоритмом (без bin)
+def norm_number(a):
+    list_a = list(a)
+    list_a.reverse()
+    result = 0
+    index_list_a = 0
+    for i in list_a:
+        result += int(i) * (2 ** index_list_a)
+        index_list_a += 1
+    return result
+
+
+print(norm_number('11001'))
